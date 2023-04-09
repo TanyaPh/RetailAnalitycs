3c61fbf27349d465a5047554695cef7efbea7ce7
v3ssel <v3ssels@pm.me>
2023-04-09 22:13:21 +0300

    P2. PurchaseHistory View

--- src/part2_purchase_history.sql
+++ src/part2_purchase_history.sql
@@ -1,13 +1,17 @@
 CREATE OR REPLACE VIEW PurchaseHistory AS
-    SELECT * FROM checks
-;
+    SELECT DISTINCT pi.customer_id
+                  , t.transaction_id
+                  , t.transaction_datetime
+                  , p.group_id
+                  , sum(s.sku_purchase_price * c2.sku_amount) AS Group_Cost
+                  , sum(c2.sku_summ) AS Group_Summ
+                  , sum(c2.sku_summ_paid) AS Group_Summ_Paid
+    FROM personinformation AS pi
+             JOIN cards c on pi.customer_id = c.customer_id
+             JOIN transactions t on c.customer_card_id = t.customer_card_id
+             JOIN checks c2 on t.transaction_id = c2.transaction_id
+             JOIN productgrid p on p.sku_id = c2.sku_id
+             JOIN stores s on s.transaction_store_id = t.transaction_store_id AND p.sku_id = s.sku_id
+    GROUP BY pi.customer_id, t.transaction_id, t.transaction_datetime, p.group_id;
 
-SELECT pi.customer_id, t.transaction_id, t.transaction_datetime, c2.sku_id, p.group_id
-     , s.sku_purchase_price, c2.sku_summ, c2.sku_summ_paid
-FROM personinformation AS pi
-JOIN cards c on pi.customer_id = c.customer_id
-JOIN transactions t on c.customer_card_id = t.customer_card_id
-JOIN checks c2 on t.transaction_id = c2.transaction_id
-JOIN productgrid p on c2.sku_id = p.sku_id
-JOIN stores s on s.transaction_store_id = t.transaction_store_id
-ORDER BY pi.customer_id, p.group_id
+SELECT * FROM PurchaseHistory
